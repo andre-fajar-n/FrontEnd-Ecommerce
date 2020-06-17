6dac43121165b6220fd6f93cde5bbf34dc63001b
andre-fajar-n <afajar@alterra.id>
2020-06-17 07:54:54 +0700

    edit footer

--- frontend_ecommerce/src/components/Footer.jsx
+++ frontend_ecommerce/src/components/Footer.jsx
@@ -3,24 +3,31 @@ import React from "react"
 const Footer = () => {
   return (
     <footer>
-      <div className="row m-0">
-        <div className="col-md-3">
-          <h1>Nama Perusahaan</h1>
+      <div className="row m-0 pt-5 text-center">
+        <div className="col-md-4 pl-5 pr-5">
+          <strong>Andreino</strong>
+          <p>Tentang Kami</p>
+          <p>Karir</p>
+          <p>Hubungi Kami</p>
         </div>
-        <div className="col-md-3">
-          <h1>Pembeli</h1>
+        <div className="col-md-4 pl-5 pr-5">
+          <strong>Informasi Layanan</strong>
+          <p>Syarat Penjual</p>
+          <p>Syarat Pembeli</p>
         </div>
-        <div className="col-md-3">
-          <h1>Penjual</h1>
-        </div>
-        <div className="col-md-3">
-          <h1>Ikuti Kami</h1>
+        <div className="col-md-4 pl-5 pr-5">
+          <strong>Ikuti Kami</strong>
+          <p><i class="fab fa-twitter-square"></i>&nbsp; Twitter</p>
+          <p><i class="fab fa-facebook-square"></i>&nbsp; Facebook</p>
+          <p><i class="fab fa-instagram-square"></i>&nbsp; Instagram</p>
+          <p><i class="fab fa-line"></i>&nbsp; Line</p>
+          <p><i class="fab fa-linkedin"></i>&nbsp; LinkedIn</p>
         </div>
       </div>
       <div className="text-center">
         <img src={require("../logos/logo_transparent.png")} alt="" width="100" height="100" />
+        <p className="text-center">Copyright</p>
       </div>
-      <h1 className="text-center">Copyright</h1>
     </footer>
   )
 }
